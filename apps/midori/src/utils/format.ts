c0012839b371382d9ae7f797dd59d92314be3cc4
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-08-05 22:37:09 +0700

    feat(format): update cn function to accept boolean values in class list

--- apps/midori/src/utils/format.ts
+++ apps/midori/src/utils/format.ts
@@ -1,5 +1,5 @@
 export const format_name = (name: string): string =>
 	name.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, (s) => s.toUpperCase());
 
-export const cn = (...classes: (string | undefined)[]): string =>
+export const cn = (...classes: (string | boolean | undefined)[]): string =>
 	classes.filter(Boolean).join(" ");
