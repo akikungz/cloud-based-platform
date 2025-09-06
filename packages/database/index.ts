e0252de5ba00240317905a753fb0d5b8e6429057
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-09-06 23:54:22 +0700

    feat(database): add mock database instance with schemas for testing

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from "drizzle-orm/bun-sql";
 import { record } from "@elysiajs/opentelemetry";
 
+import * as better_auth from "database/schema/auth/better-auth";
+import * as user from "database/schema/auth/user";
+import * as instance from "database/schema/core/instances";
+import * as network from "database/schema/core/network";
+import * as samester from "database/schema/core/samester";
+
 /**
  * Create a database connection.
  * @param uri Database connection string
@@ -10,3 +16,13 @@ export const db = (uri: string) => record("db.connection", () => drizzle(uri));
 
 /** Database instance type */
 export type DB = ReturnType<typeof db>;
+
+export const mock_db = drizzle.mock({
+  schema: {
+    ...better_auth,
+    ...user,
+    ...instance,
+    ...network,
+    ...samester,
+  }
+});
