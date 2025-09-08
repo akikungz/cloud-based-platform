6cd176e0a07624fb34394bb9b451e73426b18175
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-09-08 22:41:49 +0700

    refactor(database): remove unused staff-related functions and update Prisma schema for instance and IP address relations

--- packages/database/functions/auth/staff.ts
+++
@@ -1,6 +0,0 @@
-import type { PrismaDB } from "database";
-
-export const check_staff = async (db: PrismaDB, id: string) => {
-  const count = await db.staff_list.count({ where: { user_id: id } });
-  return count === 1;
-}
