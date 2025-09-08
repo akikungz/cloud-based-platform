bfa30006856b928c0e2f4f41ca833a8475ede0f0
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-09-08 13:56:31 +0700

    fix(callback): specify return type for create_callback function

--- packages/utils/functions/callback.ts
+++ packages/utils/functions/callback.ts
@@ -1,7 +1,7 @@
 export const create_callback =
   async <E, T>(
     callback: () => T | Promise<T>,
-  ) => {
+  ): Promise<[E | null, T | null]> => {
     try {
       const result = await callback();
       return [null, result] as [E | null, T | null];
